210d7a85008c143734dd876d39b69eee3a7f0268
ozzeke <60427475+ozzeke@users.noreply.github.com>
2020-05-18 15:00:51 +0200

    Add files via upload

---
+++ MyBankOOP/MyBankOop/BankAccount.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBankOop
+{
+	public class BankAccount
+	{
+		public string Number { get; }
+
+		public string Owner { get; set; }
+
+		public decimal Balance {
+			get
+			{
+				decimal balance = 0;
+				foreach (var item in allTransactions)
+				{
+					balance = balance + item.Amount;
+					
+				}
+				return balance;
+			
+			}
+		}
+
+		private static int accountNumberSeed = 1234567890;
+
+		private List<Transactions> allTransactions = new List<Transactions>();
+
+		public BankAccount(string name,decimal initialBalance) 
+		{
+			this.Owner = name;
+
+			MakeDeposit(initialBalance, DateTime.Now, "initial balance");
+			
+			this.Number = accountNumberSeed.ToString();
+			accountNumberSeed++;
+			
+		}
+
+		public void MakeDeposit(decimal amount, DateTime date, string note)
+		{
+			if (amount <= 0)
+			{
+				throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+				
+			}
+			var deposit = new Transactions(amount, date, note);
+			allTransactions.Add(deposit);
+		}
+
+		public void MakeWithdrawl(decimal amount, DateTime date, string note)
+		{
+			if (amount <= 0)
+			{
+				throw new ArgumentOutOfRangeException(nameof(amount), "amount of withdrawls must be positive");
+			}
+			if (Balance - amount < 0)
+			{
+				throw new InvalidOperationException("not sufficient funds for this withdrawl");
+			}
+			var withdrawl = new Transactions(-amount, date, note);
+			allTransactions.Add(withdrawl);
+		}
+
+		public string GetAccountHistory()
+		{	
+			var report = new StringBuilder();
+
+
+			//header
+			report.AppendLine("Date\tAmount\tNote");
+			foreach (var item in allTransactions)
+			{
+				//ROWS
+				report.AppendLine($"{item.Date.ToShortDateString()}\t {item.Amount}\t{item.Notes}");
+			}
+			return report.ToString();
+		}
+
+
+	}
+}
---
+++ MyBankOOP/MyBankOop/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBankOop
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			var account = new BankAccount("Sam", 2000);
+			Console.WriteLine($"Account {account.Number} is aangemaakt voor {account.Owner} met {account.Balance}");
+			
+			account.MakeWithdrawl(89, DateTime.Now, "PC case");
+			/*
+			Console.WriteLine(account.Balance);
+			*/
+
+			account.MakeWithdrawl(50, DateTime.Now, "game");
+			/*
+			Console.WriteLine(account.Balance);
+			*/
+			
+
+			Console.WriteLine(account.GetAccountHistory());
+
+			 
+
+			/*
+
+			//negative balance
+
+			try
+			{
+				account.MakeWithdrawl(4000, DateTime.Now, "attempt to overdraw");
+			}
+			catch (InvalidOperationException e)
+			{
+
+				Console.WriteLine("exeption caught trying to overdraw");
+				Console.WriteLine(e.ToString());
+			}
+
+
+
+			//positive balance
+			try
+			{
+				var invalidAccount = new BankAccount("invalid", -55);
+			}
+			catch (ArgumentOutOfRangeException e)
+			{
+				Console.WriteLine("exeption caught creating with negative balance");
+				Console.WriteLine(e.ToString());
+
+			}
+			*/
+		}
+	}
+}
